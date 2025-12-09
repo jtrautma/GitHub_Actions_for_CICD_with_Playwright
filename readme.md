acd5e83d969d0dd716d7d5207bc0f8ba63509bf8
jtrautma <trautmann.jan@gmx.de>
2025-12-09 16:02:58 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# GitHub Actions for CI/CD with Playwright [Python]
+
+<br>
+
+This repository serves as a *template* on how to set up a CI/CD Process using **GitHub Actions**. The tests to be executed are Playwright tests written in Python using the pytest framework.The test execution is triggered upon a `push` to the `Master` branch.
---
+++ tests/test_playwright_docs.py
@@ -0,0 +1,13 @@
+from playwright.sync_api import Page, expect
+
+def test_successful_playwright_docs_page(page: Page) :
+    page.goto('https://playwright.dev/python/')
+    headline = page.locator('h1.hero__title')
+    expect(headline).to_have_text('Playwright enables reliable end-to-end testing for modern web apps.')
+
+'''
+def test_failed_playwright_docs_page(page: Page) :
+    page.goto('https://playwright.dev/python/')
+    headline = page.locator('h1.hero__title')
+    expect(headline).not_to_contain_text('reliable')
+'''
